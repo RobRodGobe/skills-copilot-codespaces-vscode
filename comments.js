5362a2bd59d2ce5ed060df73fa33aee966defc21
Roberto Rodriguez <rjre.90@gmail.com>
2023-11-13 00:50:08 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+// Create web server
+//==========================================
+
+// Import module
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/comment.js');
+
+// Handle request
+router.get('/', commentController.comment_list);
+router.get('/create', commentController.comment_create_get);
+router.post('/create',commentController.comment_create_post);
+router.get('/:id/delete', commentController.comment_delete, id);
