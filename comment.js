dc76360b34de7f842b3e73cab7d415db9ac62d9b
Roberto Rodriguez <rjre.90@gmail.com>
2023-11-13 00:38:16 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,50 +1,13 @@
+// Create web server for comment
+//==========================================
+
+// Import module
 const express = require('express');
-const app = express();
-
-// Middleware to parse JSON
-app.use(express.json());
-
-// Simulated database (in-memory data)
-const comments = [
-  { id: 1, text: 'This is a great post!' },
-  { id: 2, text: 'I have a comment too!' },
-];
-
-// Comment Controller
-const commentController = {
-  getAllComments: (req, res) => {
-    res.json(comments);
-  },
-  getCommentById: (req, res) => {
-    const id = parseInt(req.params.id);
-    const comment = comments.find((c) => c.id === id);
-
-    if (comment) {
-      res.json(comment);
-    } else {
-      res.status(404).json({ message: 'Comment not found' });
-    }
-  },
-  createComment: (req, res) => {
-    const { text } = req.body;
-    const newComment = { id: comments.length + 1, text };
-    comments.push(newComment);
-    res.status(201).json(newComment);
-  },
-};
-
-// Routes
 const router = express.Router();
+const commentController = require('../controllers/comment.js');
 
-router.get('/comments', commentController.getAllComments);
-router.get('/comments/:id', commentController.getCommentById);
-router.post('/comments', commentController.createComment);
-
-// Use the router for all routes starting with /api
-app.use('/api', router);
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// Handle request
+router.get('/', commentController.comment_list);
+router.get('/create', commentController.comment_create_get);
+router.post('/create',commentController.comment_create_post);
+router.get('/:id/delete', commentController.comment_delete, id);
